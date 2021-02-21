d7359483ea2468b8dbb44c5a2111283152e0a1c1
WobbyChip <66483092+WobbyChip@users.noreply.github.com>
2021-02-22 01:47:48 +0200

    Minecraft Plugins

---
+++ AntiEndermanGrief/src/me/wobbychip/antiendermangrief/Main.java
@@ -0,0 +1,25 @@
+package me.wobbychip.antiendermangrief;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&9[AntiEndermanGrief] AntiEndermanGrief has loaded!"));
+	}
+
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+		if (event.getEntityType() == EntityType.ENDERMAN) {
+			event.setCancelled(true);
+		}
+	}
+}
