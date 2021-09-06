67870d70bb40198929f420386663a5e7384da4d2
WobbyChip <wobbychip.simik@gmail.com>
2021-09-06 23:56:48 +0300

    1

--- RecallPotion/src/me/wobbychip/recallpotion/BrewRegister.java
+++ RecallPotion/src/me/wobbychip/recallpotion/BrewRegister.java
@@ -2,9 +2,12 @@ package me.wobbychip.recallpotion;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionType;
 
 import net.minecraft.core.IRegistry;
@@ -12,14 +15,23 @@ import net.minecraft.resources.MinecraftKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.alchemy.PotionBrewer;
 import net.minecraft.world.item.alchemy.PotionRegistry;
+import net.minecraft.world.item.alchemy.PotionUtil;
 
 public class BrewRegister {
 	public static MinecraftKey minecraftKey(Material material) {
 		return new MinecraftKey(material.getKey().getNamespace(), material.getKey().getKey());
 	}
 
-	public static MinecraftKey minecraftKey(PotionType potionType) {
-		return MinecraftKey.a(potionType.toString().toLowerCase());
+	public static net.minecraft.world.item.ItemStack asNMSCopy(ItemStack itemStack) {
+    	try {
+    		String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    		Class<?> CraftItemStack = Class.forName("org.bukkit.craftbukkit." + version + ".inventory.CraftItemStack");
+    		Method method = CraftItemStack.getDeclaredMethod("asNMSCopy", ItemStack.class);
+    		return (net.minecraft.world.item.ItemStack) method.invoke(method, itemStack);
+    	} catch (ClassNotFoundException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException e) {
+        	e.printStackTrace();
+        	return null;
+		}
 	}
 
 	//This function is ignoring legacy items since their anyways are deprecated
@@ -34,42 +46,33 @@ public class BrewRegister {
 		return result[0];
 	}
 
-	//This is not working always since potion names doesn't match
 	public static PotionRegistry findPotion(PotionType potionType) {
-		PotionRegistry[] result = {null};
+		ItemStack potionItemStack = new ItemStack(Material.POTION);
+		PotionMeta potionMeta = (PotionMeta) potionItemStack.getItemMeta();
+		potionMeta.setBasePotionData(new PotionData(potionType));
+		potionItemStack.setItemMeta(potionMeta);
 
-        //IRegistry.POTION
-        IRegistry.aa.getOptional(minecraftKey(potionType)).ifPresent((potion) -> {
-        	result[0] = potion;
-        });
+		net.minecraft.world.item.ItemStack item = asNMSCopy(potionItemStack);
+		if (item == null) { return null; }
 
-		return result[0];
+		return PotionUtil.d(item);
 	}
 
-	public static boolean isRegisterBrewMethod(Method method) {
-		if ((method == null) || (method.getParameterCount() != 3)) { return false; }
-
-		Type[] parameters = method.getParameterTypes();
-		if (!parameters[0].equals(PotionRegistry.class)) { return false; }
-		if (!parameters[1].equals(Item.class)) { return false; }
-		if (!parameters[2].equals(PotionRegistry.class)) { return false; }
-
-		return true;
-	}
-
-	public static Method findRegisterBrewMethod() {
-		for (Method method : PotionBrewer.class.getDeclaredMethods()) {
-			if (isRegisterBrewMethod(method)) { return method; }
+	public static Method getRegisterBrewMethod() {
+		try {
+			Method method = PotionBrewer.class.getDeclaredMethod("a", PotionRegistry.class, Item.class, PotionRegistry.class);
+			return method;
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			return null;
 		}
-
-		return null;
 	}
 
-	public static boolean registerBrewRecipe(PotionType base, Material ingredient, PotionType result ) {
+	public static boolean registerBrewRecipe(PotionType base, Material ingredient, PotionType result) {
 		Item potionIngredient = findItem(ingredient);
 		if (potionIngredient == null) { return false; }
 
-		Method method = findRegisterBrewMethod();
+		Method method = getRegisterBrewMethod();
 		if (method == null) { return false; }
 
 		PotionRegistry potionBase = findPotion(base);
