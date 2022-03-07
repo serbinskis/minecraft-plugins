7699ba500a3a2bab3e37f101eaa6b903f37e55a5
WobbyChip <wobbychip.simik@gmail.com>
2022-03-07 20:50:44 +0200

    1

--- SMPTweaks/src/me/wobbychip/smptweaks/custom/pvpdropinventory/Events.java
+++ SMPTweaks/src/me/wobbychip/smptweaks/custom/pvpdropinventory/Events.java
@@ -2,13 +2,20 @@ package me.wobbychip.smptweaks.custom.pvpdropinventory;
 
 import java.util.Arrays;
 
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.AreaEffectCloud;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.projectiles.ProjectileSource;
 
 import me.wobbychip.smptweaks.Utils;
 
@@ -36,13 +43,43 @@ public class Events implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
 		if (!(event.getEntity() instanceof Player)) { return; }
-		if (!(event.getDamager() instanceof Player)) { return; }
-		if (event.getEntity().getUniqueId().equals(event.getDamager().getUniqueId())) { return; }
+		addPlayers((Player) event.getEntity(), getAttacker(event.getDamager()));
+	}
 
-		PvPDropInventory.timer.addPlayer((Player) event.getEntity(), PvPDropInventory.timeout);
-		PvPDropInventory.timer.sendActionMessage((Player) event.getEntity());
+	public Player getAttacker(Entity entity) {
+		if (entity instanceof Player) { return ((Player) entity); }
 
-		PvPDropInventory.timer.addPlayer((Player) event.getDamager(), PvPDropInventory.timeout);
-		PvPDropInventory.timer.sendActionMessage((Player) event.getDamager());
+		if ((entity instanceof Projectile)) {
+			ProjectileSource attacker = ((Projectile) entity).getShooter();
+			if ((attacker instanceof Player)) { return ((Player) attacker); }
+		}
+
+		if ((entity instanceof AreaEffectCloud)) {
+			ProjectileSource attacker = ((AreaEffectCloud) entity).getSource();
+			if ((attacker instanceof Player)) { return ((Player) attacker); }
+		}
+
+		if ((entity instanceof TNTPrimed)) {
+			Entity attacker = ((TNTPrimed) entity).getSource();
+			if ((attacker instanceof Player)) { return ((Player) attacker); }
+		}
+
+		if ((entity instanceof Tameable)) {
+			AnimalTamer attacker = ((Tameable) entity).getOwner();
+			if ((attacker instanceof Player)) { return ((Player) attacker); }
+		}
+
+		return null;
+	}
+
+	public void addPlayers(Player player1, Player player2) {
+		if ((player1 == null) || (player2 == null)) { return; }
+		if (player1.getUniqueId().equals(player2.getUniqueId())) { return; }
+
+		PvPDropInventory.timer.addPlayer(player1, PvPDropInventory.timeout);
+		PvPDropInventory.timer.sendActionMessage(player1);
+
+		PvPDropInventory.timer.addPlayer(player2, PvPDropInventory.timeout);
+		PvPDropInventory.timer.sendActionMessage(player2);
 	}
 }
