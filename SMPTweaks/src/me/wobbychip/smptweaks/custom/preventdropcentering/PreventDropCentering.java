5f0d0d0268c91bef4b02108129103fa1b61baffa
WobbyChip <66483092+WobbyChip@users.noreply.github.com>
2022-06-12 02:15:34 +0300

    Update PreventDropCentering.java

--- SMPTweaks/src/me/wobbychip/smptweaks/custom/preventdropcentering/PreventDropCentering.java
+++ SMPTweaks/src/me/wobbychip/smptweaks/custom/preventdropcentering/PreventDropCentering.java
@@ -3,6 +3,9 @@ package me.wobbychip.smptweaks.custom.preventdropcentering;
 import org.bukkit.Bukkit;
 
 import me.wobbychip.smptweaks.Main;
+import me.wobbychip.smptweaks.PaperUtils;
+import me.wobbychip.smptweaks.Utils;
+import me.wobbychip.smptweaks.custom.globaltrading.Events;
 import me.wobbychip.smptweaks.tweaks.CustomTweak;
 
 public class PreventDropCentering extends CustomTweak {
@@ -10,8 +13,12 @@ public class PreventDropCentering extends CustomTweak {
 		super("PreventDropCentering");
 
 		if (this.isEnabled()) {
-			Bukkit.getPluginManager().registerEvents(new Events(), Main.plugin);
-			this.printEnabled();
+			if (PaperUtils.isPaper) {
+				Bukkit.getPluginManager().registerEvents(new Events(), Main.plugin);
+				this.printEnabled();
+			} else {
+				Utils.sendMessage("[PreventDropCentering] Ment only for PaperMC.");
+			}
 		} else {
 			this.printDisabled();
 		}
